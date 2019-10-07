d79050aa49cc9a88ef13f535ffa419a0f8e60431
mweerts <maximeweerts@hotmail.be>
2019-10-07 23:38:59 +0200

    modif strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,23 +6,23 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 22:17:42 by mweerts           #+#    #+#             */
-/*   Updated: 2019/10/07 22:27:00 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/10/07 23:38:35 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int i;
 
 	i = 0;
-	while (src[i] && i + 1 < dstsize)
+	while (src[i] && i + 1 < size)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (dstsize > 0)
+	if (size > 0)
 		dst[i] = '\0';
 	return (ft_strlen(src));
 }
