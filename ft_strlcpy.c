7953620790a8b76283c96614f63414b7b80460e6
mweerts <maximeweerts@hotmail.be>
2019-10-08 15:13:37 +0200

    norminette

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 22:17:42 by mweerts           #+#    #+#             */
-/*   Updated: 2019/10/07 23:38:35 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/10/08 15:10:01 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int i;
 
