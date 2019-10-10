253c15b9b1b1ce9bc89dbd678bdb5bb7d1ee2a39
mweerts <maximeweerts@hotmail.be>
2019-10-10 14:33:33 +0200

    ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 14:25:34 by mweerts           #+#    #+#             */
+/*   Updated: 2019/10/10 14:33:19 by mweerts          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*substr;
+	
+	i = 0;
+	substr = malloc(sizeof(char) * len + 1);
+	if (substr == NULL)
+		return (NULL);
+	while (i < len && s[start])
+	{
+		substr[i] = s[start];
+		i++;
+		start++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
