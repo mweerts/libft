cca504661f5de1403db69648756beb0ac3f5fd20
mweerts <maximeweerts@hotmail.be>
2019-10-17 00:54:15 +0200

    modif strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 21:26:46 by mweerts           #+#    #+#             */
-/*   Updated: 2019/10/17 00:51:48 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/10/17 00:54:01 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,9 @@ char	*ft_strmapi(char *s, char (*f)(unsigned int, char))
 	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (str == NULL)
 		return (NULL);
-	str = ft_memcpy(str, s, ft_strlen(s));
 	while (str[i])
 	{
-		str[i] = f(i, str[i]);
+		str[i] = f(i, s[i]);
 		i++;
 	}
 	str[i] = '\0';
