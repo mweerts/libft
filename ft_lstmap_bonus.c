32f2b851d27251fe203243460a7cfd89a5b5d857
mweerts <maximeweerts@hotmail.be>
2019-10-20 17:58:12 +0200

    ft_lstmap

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/20 17:46:09 by mweerts           #+#    #+#             */
+/*   Updated: 2019/10/20 17:57:37 by mweerts          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+{
+	t_list	*ret;
+
+	ret = (*f)(lst);
+	lst = lst->next;
+	while (lst)
+	{
+		ret->next = (*f)(lst);
+		ret = ret->next;
+	}
+	return (ret);
+}
