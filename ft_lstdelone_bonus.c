730f1c1378c72175c23fbdf83a148da45cc37f22
mweerts <maximeweerts@hotmail.be>
2019-10-29 19:51:26 +0100

    protec lstdelone

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 21:35:28 by mweerts           #+#    #+#             */
-/*   Updated: 2019/10/22 16:37:41 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/10/29 19:50:08 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
+	if (!lst || !del)
+		return ;
 	(*del)(lst->content);
 	free(lst);
-	lst = NULL;
 }
