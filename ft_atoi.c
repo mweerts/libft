51d0f26d0e75efb0553705d70aff40699b9d9375
mweerts <maximeweerts@hotmail.be>
2019-11-07 23:32:03 +0100

    modif atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:46:55 by mweerts           #+#    #+#             */
-/*   Updated: 2019/10/29 15:35:27 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/11/07 23:17:30 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-	int				i;
-	unsigned int	nbr;
-	int				is_neg;
+	int					i;
+	unsigned long long	nbr;
+	int					is_neg;
 
 	i = 0;
 	nbr = 0;
@@ -33,9 +33,9 @@ int	ft_atoi(const char *str)
 		i++;
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 		nbr = nbr * 10 + (str[i++] - 48);
-	if (nbr > 2147483647 && is_neg == 1)
+	if (nbr > LONG_MAX && is_neg == 1)
 		return (-1);
-	if (nbr > 2147483648 && is_neg == -1)
+	if (nbr > LONG_MAX && is_neg == -1)
 		return (0);
 	nbr *= is_neg;
 	return ((int)nbr);
