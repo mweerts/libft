d004eb3797ade10c58f47e1aa7a0cb0577f08b4a
mweerts <maximeweerts@hotmail.be>
2019-11-08 17:36:50 +0100

    modif memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char		*pdest;
 	char		*psrc;
 
+	if (s1 == s2 || n == 0)
+		return (dst);
 	pdest = (char *)dst;
 	psrc = (char *)src;
 	i = 0;
