b22ffcf63ca4fd8e379ddf1d47a7727f10340837
mweerts <maximeweerts@hotmail.be>
2019-11-11 14:10:54 +0100

    modif strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,8 +17,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t i;
 
 	i = 0;
-	if (dst == NULL || src == NULL)
+	if (dst == NULL && src == NULL)
 		return (0);
+	if (!size)
+		return (ft_strlen(src));
 	while (src[i] && i + 1 < size)
 	{
 		dst[i] = src[i];
