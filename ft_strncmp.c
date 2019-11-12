ffb9ee0bab5f63e5cff1002a8d8a5b0c72202a78
mweerts <maximeweerts@hotmail.be>
2019-11-12 23:17:54 +0100

    modif strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,12 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char *ps1;
+	unsigned char *ps2;
 
 	i = 0;
+	ps1 = (unsigned char *)s1;
+	ps2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
+	while (ps1[i] == ps2[i] && ps1[i] && ps2[i] && i < n - 1)
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (ps1[i] - ps2[i]);
 }
